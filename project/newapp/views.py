27e6164fe20fa34192ab68572209d96a4e5f0c99
2552nik <nick.muravscky2013@yandex.ru>
2021-12-12 20:33:40 +0300

    D_3

---
+++ project/newapp/views.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+
+from django.views.generic import ListView, DetailView
+from .models import Author, Category, Post, Comment
+
+
+class PostsList(ListView):
+    model = Post  # указываем модель, объекты которой мы будем выводить
+    template_name = 'posts.html'  # указываем имя шаблона, в котором будет лежать HTML,
+    # в котором будут все инструкции о том, как именно пользователю должны вывестись наши объекты
+    context_object_name = 'posts'  # это имя списка, в котором будут лежать все объекты, его надо указать,
+    # чтобы обратиться к самому списку объектов через HTML-шаблон
+    queryset = Post.objects.order_by('-dateCreation')
+
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.utcnow()  # добавим переменную текущей даты time_now
+        context['value1'] = "круто и все такое" # добавим ещё одну пустую переменную, чтобы на её примере посмотреть работу
+        return context
+
+
+
+# создаём представление, в котором будут детали конкретного отдельного товара
+class PostsDetail(DetailView):
+    model = Post  # модель всё та же, но мы хотим получать детали конкретно отдельного товара
+    template_name = 'post.html'  # название шаблона будет product.html
+    context_object_name = 'post'  # название объекта
